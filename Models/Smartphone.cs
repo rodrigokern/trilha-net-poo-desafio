8a1aedfd9e08ed1cb93d90bc5bcb9b2cf641d3a8
Rodrigo Kern <rodrigokern@gmail.com>
2024-08-20 18:01:34 -0300

    Desafio concluido

--- Models/Smartphone.cs
+++ Models/Smartphone.cs
@@ -3,22 +3,36 @@ namespace DesafioPOO.Models
     public abstract class Smartphone
     {
         public string Numero { get; set; }
-        // TODO: Implementar as propriedades faltantes de acordo com o diagrama
+        private string Modelo { get; set; }
+        private string IMEI { get; set; }
+        private int Memoria { get; set; }
 
-        public Smartphone(string numero)
+        public Smartphone(string numero, string modelo, string imei, int memoria)
         {
             Numero = numero;
-            // TODO: Passar os parâmetros do construtor para as propriedades
+            Modelo = modelo;
+            IMEI = imei;
+            Memoria = memoria;
         }
 
         public void Ligar()
         {
-            Console.WriteLine("Ligando...");
+            Console.WriteLine($"Ligando pelo número: [{Numero}]");
         }
 
         public void ReceberLigacao()
         {
-            Console.WriteLine("Recebendo ligação...");
+            Console.WriteLine($"Recebendo ligação pelo número: [{Numero}]");
+        }
+
+        public void DebugInfo()
+        {
+            string className = this.GetType().Name;
+            Console.WriteLine($"DebugInfo for [{className}]");
+            Console.WriteLine($"Numero: [{Numero}]");
+            Console.WriteLine($"Modelo: [{Modelo}]");
+            Console.WriteLine($"IMEI: [{IMEI}]");
+            Console.WriteLine($"Memoria: [{Memoria}]");
         }
 
         public abstract void InstalarAplicativo(string nomeApp);
--- Program.cs
+++ Program.cs
@@ -1,3 +1,15 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Smartphone Nokia:");
+Smartphone nokia = new Nokia("123-456-789", "Modelo 1", "987654321", 512);
+nokia.Ligar();
+nokia.InstalarAplicativo("Telegram");
+nokia.DebugInfo();
+
+Console.WriteLine();
+
+Console.WriteLine("Smartphone iPhone:");
+Smartphone iphone = new Iphone("987-654-321", "Modelo 2", "123456789", 128);
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Whatsapp");
+iphone.DebugInfo();
